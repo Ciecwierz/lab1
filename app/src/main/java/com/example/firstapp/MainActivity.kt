0911b2e6d8df2ff363f2c3ca70800073c4c8efdb
Ciecwierz <jakub_ciecwierz016@o2.pl>
2023-03-20 00:10:21 +0100

    dokonczenie zadania z przyciskami w domu

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,15 +1,43 @@
 package com.example.firstapp
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity: AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate (savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val buttonLog = findViewById<Button>(R.id.buttonLog)
+        val buttonToast = findViewById<Button>(R.id. buttonToast)
+        val buttonRewrite =findViewById<Button>(R.id. buttonRewrite)
+        val editTextMessage = findViewById<EditText>(R.id. editTextMessage)
+        buttonLog.setOnClickListener{
+
+
+            val text = editTextMessage.text.toString()
+            Log.d("FirstApp", "$text")
+
+        }
+
+        buttonToast.setOnClickListener{
+            val text = editTextMessage.text.toString()
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
+
+        }
+        buttonRewrite.setOnClickListener {
+            val textViewRewriteText = findViewById<TextView>(R.id. textViewRewriteText)
+            textViewRewriteText.text = editTextMessage.text.toString()
+
+        }
 
     }
 }
